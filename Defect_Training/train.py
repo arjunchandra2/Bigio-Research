538fd45f8d0f3557110911895afb142f4abf9e8e
arjunchandra2 <ac25@bu.edu>
2024-03-23 17:44:51 -0400

    Setting up training

---
+++ Defect_Training/train.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Training script for defect detection
+Author: Arjun Chandra 
+
+@software{yolov8_ultralytics,
+  author = {Glenn Jocher and Ayush Chaurasia and Jing Qiu},
+  title = {Ultralytics YOLOv8},
+  version = {8.0.0},
+  year = {2023},
+  url = {https://github.com/ultralytics/ultralytics},
+  orcid = {0000-0001-5950-6979, 0000-0002-7603-6750, 0000-0003-3783-7069},
+  license = {AGPL-3.0}
+}
+"""
+
+import os 
+from os import listdir
+import torch
+import torchvision
+from IPython import display
+from IPython.display import display, Image
+from ultralytics import YOLO
+
+d_path = "/projectnb/npbssmic/ac25/Defect_Training/data.yaml"
+
+model = YOLO("yolov8n.yaml")
+
+results = model.train(data = d_path, 
+                      epochs = 100,
+                      imgsz = 640, 
+                      save = True
+                      plots = True)
